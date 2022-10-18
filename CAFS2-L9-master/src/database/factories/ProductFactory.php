9951381356dfc94e1274ec50d619e7bd3eb494c2
Tomas <tj4305@gmail.com>
2022-10-18 11:27:52 +0300

    Added product seeder

---
+++ CAFS2-L9-master/src/database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\ProductCategory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'is_active' => fake()->boolean(),
+            'name' => fake()->words(3, true),
+            'description' => fake()->text(200),
+            'stock' => fake()->numberBetween(1, 10)
+        ];
+    }
+}

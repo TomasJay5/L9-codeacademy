137bde0b49bbd66e58bb99be6b40a21ea82e413d
Tomas <tj4305@gmail.com>
2022-10-24 11:14:42 +0300

    Query builder

--- CAFS2-L9-master/src/routes/web.php
+++ CAFS2-L9-master/src/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/count', [Controllers\Products\ProductController::class, 'count']);
+
+Route::get('/categories', [Controllers\Products\ProductController::class, 'categories']);
+
+Route::get('/byname', [Controllers\Products\ProductController::class, 'byName']);
+
+Route::get('/transactions', [Controllers\Products\ProductController::class, 'transactions']);
